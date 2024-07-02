cac2d84a3fd8eb37a24e759cb77993c2b6f0d603
brian triplett <btriplett1990@yahoo.com>
2024-07-02 19:44:03 -0400

    swap out kitty for alacritty

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # install packages
-sudo pacman -Sy firefox neovim nodejs kitty awesome xclip xcompmgr feh npm rofi -y
+sudo pacman -Sy firefox neovim nodejs alacritty awesome xclip xcompmgr feh npm rofi -y
 
 # create config directory
 mkdir ~/.config 2>/dev/null
@@ -9,7 +9,7 @@ mkdir ~/.config 2>/dev/null
 # remove any existing config files
 rm -rf ~/.config/awesome
 rm ~/.bashrc
-rm -rf ~/.config/kitty
+rm -rf ~/.config/alacritty
 rm -rf ~/.config/nvim
 rm -rf ~/.config/rofi
 rm -rf ~/Documents/Scripts
@@ -17,7 +17,7 @@ rm -rf ~/Documents/Scripts
 # link config files to dotfiles directory
 ln -sf ~/.dotfiles/awesome ~/.config/awesome
 ln -sf ~/.dotfiles/bashrc ~/.bashrc
-ln -sf ~/.dotfiles/kitty ~/.config/kitty
+ln -sf ~/.dotfiles/alacritty ~/.config/alacritty
 ln -sf ~/.dotfiles/nvim ~/.config/nvim
 ln -sf ~/.dotfiles/rofi ~/.config/rofi
 ln -sf ~/.dotfiles/Scripts ~/Documents/Scripts
